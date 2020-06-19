33884265c5a316f5d25c895c78858dd374f91f43
Cezar Sa Espinola <cezarsa@gmail.com>
2020-06-19 01:42:46 -0300

    Migrate to platformio

--- include/base.hpp
+++ include/base.hpp
@@ -1,8 +1,8 @@
 #ifndef _BASE_HPP_
 #define _BASE_HPP_
 
-#include <ArduinoMqttClient.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <WiFiManager.h>
 
 const int MAX_TOPIC_LENGTH = 64;
@@ -10,7 +10,7 @@ const int MAX_TOPIC_LENGTH = 64;
 class base {
  private:
   WiFiClient wifi;
-  MqttClient mqtt;
+  PubSubClient mqtt;
   WiFiManager wifiManager;
   WiFiManagerParameter custom_mqtt_topic;
 
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+// #include "base.hpp"
+
+// base app;
+
+void setup() {
+  //   if (!app.connect()) {
+  //     return;
+  //   }
+}
+
+void loop() {}
